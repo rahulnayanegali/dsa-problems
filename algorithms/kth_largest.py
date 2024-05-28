8f60f8ca0c3b9b62d472bd932b3fab3961c9349c
Rahul Nayanegali <rahulnayanegali@gmail.com>
2024-05-28 00:37:50 -0400

    kth largest element modified.

--- algorithms/kth_largest.py
+++ algorithms/kth_largest.py
@@ -1,15 +1,17 @@
 import heapq
 
+
 def findKthLargest(nums, k):
     k_nums = nums[:k]
     heapq.heapify(k_nums)
 
-    for i in range(k, len(nums)-k):
-        heapq.heappop(k_nums)
-        heapq.heappush(k_nums, nums[i])
+    for i in range(k, len(nums)):
+        if nums[i] > k_nums[0]:
+            heapq.heappop(k_nums)
+            heapq.heappush(k_nums, nums[i])
 
-    return heapq.heappop(k_nums)
+    return k_nums[0]
 
 
-print(findKthLargest([5, 7, 9, 1, 3], 2))
-#9,7,5,3,1
+print(findKthLargest([3, 2, 1, 5, 6, 4], 2))
+# 9,7,5,3,1
