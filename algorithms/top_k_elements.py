a5010eaa26304f5426011c99124a0ec69be3d0dc
Rahul Nayanegali <rahulnayanegali@gmail.com>
2024-05-28 00:39:36 -0400

    top k elements.

---
+++ algorithms/top_k_elements.py
@@ -0,0 +1,17 @@
+import heapq
+
+
+def top_k_elements(nums, k):
+    k_nums = nums[:k]
+    heapq.heapify(k_nums)
+
+    for i in range(k, len(nums)):
+        if nums[i] > k_nums[0]:
+            heapq.heappop(k_nums)
+            heapq.heappush(k_nums, nums[i])
+
+    return k_nums
+
+
+print(top_k_elements([3, 2, 1, 5, 6, 4], 2))
+# 9,7,5,3,1
